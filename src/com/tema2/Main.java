ce9f8a1af9b0817a6899ca9c007068886dcfd383
Rick1124 <38147073+Rick1124@users.noreply.github.com>
2022-08-18 00:43:25 -0500

    Final

--- src/com/tema2/Main.java
+++ src/com/tema2/Main.java
@@ -1,2 +1,18 @@
-package com.tema2;public class Main {
+package com.tema2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.close();
+        double finalPrice = addIVA(n);
+        System.out.println(finalPrice);
+    }
+    static double addIVA(double value) {
+        double iva = value * 0.15;
+        value += iva;
+        return value;
+    }
 }
